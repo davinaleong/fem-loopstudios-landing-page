bbb3c84459d291520f057c761c3ee790f7d5a895
Davina Leong <davina.leong.sy@gmail.com>
2022-08-26 09:43:37 +0800

    Done with the header

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-console.log(`Script loaded`)
+const primaryHeaderEl = document.querySelector(`[data-primary-header]`)
+const btnPrimaryToggleEl = document.querySelector(`[data-primary-toggle]`)
+
+const openAttribute = `data-open`
+
+btnPrimaryToggleEl.addEventListener(`click`, (e) => {
+  primaryHeaderEl.toggleAttribute(openAttribute)
+})
