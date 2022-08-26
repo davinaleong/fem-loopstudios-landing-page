fd76a5c9d7e43a74bbddce04fbd2eceeedb19d78
Davina Leong <davina.leong.sy@gmail.com>
2022-08-26 10:01:08 +0800

    Add scroll effect to header

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,16 @@ const primaryHeaderEl = document.querySelector(`[data-primary-header]`)
 const btnPrimaryToggleEl = document.querySelector(`[data-primary-toggle]`)
 
 const openAttribute = `data-open`
+const opaqueAttribute = `data-opaque`
+
+window.addEventListener("scroll", () => {
+  const yPos = window.scrollY
+  if (yPos > 300) {
+    primaryHeaderEl.setAttribute(opaqueAttribute, true)
+  } else {
+    primaryHeaderEl.removeAttribute(opaqueAttribute)
+  }
+})
 
 btnPrimaryToggleEl.addEventListener(`click`, (e) => {
   primaryHeaderEl.toggleAttribute(openAttribute)
